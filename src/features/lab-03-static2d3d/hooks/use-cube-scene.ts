a432e5143271b89d2ee8fa433dc44a2d7b90de71
heyra <zalomnova.eliz@gmail.com>
2024-03-12 00:28:28 +0300

    feat: add rotated cube scene

---
+++ src/features/lab-03-static2d3d/hooks/use-cube-scene.ts
@@ -0,0 +1,73 @@
+import GLCanvas from 'src/shared/components/webgl/GLCanvas.vue';
+import { setupBuffers, setupShaderProgram, setupTransformationMatrices } from 'src/utils/webgl';
+import { fragmentShaderSource, vertexShaderSource } from 'src/shared/resources/basic-shaders';
+import { identity } from 'src/shared/resources/identity';
+import { glMatrix, mat4 } from 'gl-matrix';
+import { boxIndices, boxVertices } from 'src/shared/resources/box-model';
+
+
+const useCubeScene = (glCanvas: typeof GLCanvas) => {
+  const glContext = glCanvas.glContext as WebGL2RenderingContext;
+
+  // compile shaders
+  const program = setupShaderProgram(
+    glContext,
+    vertexShaderSource,
+    fragmentShaderSource
+  ) as WebGLProgram;
+
+  // setup buffers
+  setupBuffers(glContext, program, boxVertices, boxIndices);
+
+  glContext.useProgram(program);
+
+  const zRotationMatrix = new Float32Array(16);
+  mat4.rotate(zRotationMatrix, identity,  Math.PI / 6, [0, 0, 1]);
+
+  const yRotationMatrix = new Float32Array(16);
+  mat4.rotate(yRotationMatrix, identity, Math.PI / 4, [0, 1, 0]);
+
+  const rotationMatrix = new Float32Array(16);
+  mat4.mul(rotationMatrix, yRotationMatrix, zRotationMatrix);
+
+  setupTransformationMatrices(
+    glContext,
+    program,
+    {
+      translation: identity,
+      rotation: rotationMatrix,
+      scale: identity,
+    },
+    {
+      eye: [0, 0, -8],
+      center: [0, 0, 0],
+      up: [0, 1, 0],
+    },
+    {
+      fovy: glMatrix.toRadian(45),
+      aspect: glCanvas.width / glCanvas.height,
+      nearPlane: 0.1,
+      farPlane: 100.0,
+    },
+  )
+
+  const drawScene = () => {
+    glContext.clearColor(0, 0, 0, 1.0);
+    glContext.clear(glContext.DEPTH_BUFFER_BIT | glContext.COLOR_BUFFER_BIT);
+    glContext.drawElements(glContext.TRIANGLES, boxIndices.length, glContext.UNSIGNED_SHORT, 0);
+  };
+
+  const runScene = () => {
+    drawScene()
+    requestAnimationFrame(runScene);
+  }
+
+  // end
+  return {
+    runScene
+  }
+};
+
+export {
+  useCubeScene
+}
--- src/features/lab-03-static2d3d/pages/Static2d3dPage.vue
+++ src/features/lab-03-static2d3d/pages/Static2d3dPage.vue
@@ -3,6 +3,9 @@
     <div class="row">
       <GLCanvas ref="pentagonCanvas" />
     </div>
+    <div class="row">
+      <GLCanvas ref="cubeCanvas" />
+    </div>
   </q-page>
 </template>
 
@@ -11,8 +14,10 @@ import GLCanvas from 'src/shared/components/webgl/GLCanvas.vue';
 import { onMounted, Ref, ref } from 'vue';
 import { MaybeUndefined } from 'src/shared/models/generic';
 import { usePentagonScene } from 'src/features/lab-03-static2d3d/hooks/use-pentagon-scene';
+import { useCubeScene } from 'src/features/lab-03-static2d3d/hooks/use-cube-scene';
 
 const pentagonCanvas: Ref<MaybeUndefined<typeof GLCanvas>> = ref(undefined);
+const cubeCanvas: Ref<MaybeUndefined<typeof GLCanvas>> = ref(undefined);
 
 
 const run = () => {
@@ -20,9 +25,17 @@ const run = () => {
     console.log('No canvas context found for quad scene')
   } else {
     const {
-      runScene: runQuadScene
+      runScene: runPentagonScene
     } = usePentagonScene(pentagonCanvas.value);
-    runQuadScene();
+    runPentagonScene();
+  }
+  if (!cubeCanvas.value || !cubeCanvas.value.glContext) {
+    console.log('No canvas context found for cube scene')
+  } else {
+    const {
+      runScene: runCubeScene
+    } = useCubeScene(cubeCanvas.value);
+    runCubeScene();
   }
 }
 
