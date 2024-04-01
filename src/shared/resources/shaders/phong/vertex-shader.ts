d4eaa879ae4eccc64492d9fb3fb07a8dc8471fef
heyra <zalomnova.eliz@gmail.com>
2024-04-02 00:24:21 +0300

    fix: add normal matrix (todo: move calculations to cpu)

--- src/shared/resources/shaders/phong/vertex-shader.ts
+++ src/shared/resources/shaders/phong/vertex-shader.ts
@@ -13,16 +13,19 @@ uniform mat4 mWorld;
 uniform mat4 mView;
 uniform mat4 mProj;
 
-out vec3 fragNormal;
 out vec3 fragColor;
 out vec3 fragPos;
+out vec3 fragNormal;
 
 void main() {
   vec4 vertPosition4 = vec4(vertPosition, 1.0);
   gl_Position = mProj * mView * mWorld * vertPosition4;
   fragColor = vertColor;
-  fragNormal = vertNormal;
   fragPos = vec3(mWorld * vertPosition4);
+
+  // todo: move calculation to cpu
+  mat3 normalMat = mat3(transpose(inverse(mWorld)));
+  fragNormal = normalMat * vertNormal;
 }
 `;
 
